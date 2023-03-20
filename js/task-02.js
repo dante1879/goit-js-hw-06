84e42318063e333405fae365a28f8d4a043f1758
dante1879 <sirenko.alex777@gmail.com>
2023-03-20 23:27:55 +0200

    hw2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulIngridients = document.querySelector("ul#ingredients");
+
+const liToAppend = [];
+
+ingredients.forEach((ing) => {
+  const li = document.createElement('li');
+  li.textContent = ing;
+  li.classList.add('item');
+  liToAppend.push(li);
+});
+
+ulIngridients.append(...liToAppend);
