d801fd995388cd0395c723fc818761509afcd0d4
dante1879 <sirenko.alex777@gmail.com>
2023-03-27 02:58:14 +0300

    hw8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+    const dataToSend = {}
+  const formData = new FormData(event.currentTarget);
+
+  formData.forEach((value, name) => {
+    if (value.length == 0) {
+      alert("Please fill in all form fields!");
+    } else {
+        dataToSend[name] = value;
+      } 
+  });
+
+    console.log(dataToSend)
+    
+    formRef.reset()
+}
