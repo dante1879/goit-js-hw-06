a0b7f6fe5b6d7567b034fb84eb9619991a08ee23
dante1879 <sirenko.alex777@gmail.com>
2023-03-27 03:03:22 +0300

    hw9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyRef = document.querySelector("body");
+const spanRef = document.querySelector(".color");
+const btnRef = document.querySelector(".change-color");
+
+btnRef.addEventListener("click", onBtnClick);
+
+function onBtnClick(event) {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanRef.textContent = bodyRef.style.backgroundColor;
+}
