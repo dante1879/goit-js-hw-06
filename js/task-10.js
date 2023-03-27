afa27547e50ace7d5e2df97872434fbf79977390
dante1879 <sirenko.alex777@gmail.com>
2023-03-27 03:23:23 +0300

    hw10 done

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,29 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createButtonRef = document.querySelector("button[data-create]");
+const destroyButtonRef = document.querySelector("button[data-destroy]");
+const boxesRef = document.querySelector("#boxes");
+const inputRef = document.querySelector("#controls input");
+
+createButtonRef.addEventListener("click", createBoxes);
+destroyButtonRef.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  let size = 30;
+  const amount = inputRef.value;
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.width = size + "px";
+    div.style.height = size + "px";
+    div.style.backgroundColor = getRandomHexColor();
+    boxesRef.appendChild(div);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
