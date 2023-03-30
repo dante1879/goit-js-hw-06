7cd0600459e06511d2fa21c711928f3cea10f8f4
dante1879 <sirenko.alex777@gmail.com>
2023-03-31 00:15:56 +0300

    hw8 fix

--- js/task-08.js
+++ js/task-08.js
@@ -4,18 +4,20 @@ formRef.addEventListener("submit", onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
-    const dataToSend = {}
+  const dataToSend = {};
   const formData = new FormData(event.currentTarget);
 
   formData.forEach((value, name) => {
     if (value.length == 0) {
       alert("Please fill in all form fields!");
+      return;
     } else {
-        dataToSend[name] = value;
-      } 
+      dataToSend[name] = value;
+    }
   });
+  if (dataToSend["email"] && dataToSend["password"]) {
+    console.log(dataToSend);
+  }
 
-    console.log(dataToSend)
-    
-    formRef.reset()
+  formRef.reset();
 }
